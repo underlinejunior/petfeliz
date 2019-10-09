c5fa8c01d0f1e1aec842dc6cf6b0763e7d86ad8a
Evaldo Junior <e.junior123@gmail.com>
2019-10-09 17:37:40 -0300

    alteracoes na pagina de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,27 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Validators, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
   passwordTypeInput = 'password';
   iconpassword = 'eye-off';
+  authForm: FormGroup;
+  configs = {
+    cadastrar: true,
+    action: 'Logar',
+    actionChange: 'Cadastrar'
+  };
 
-constructor( private navCtrl: NavController, ) {
+  constructor(
+    private navCtrl: NavController,
+    private fb: FormBuilder,
+  ) {
+  }
 
-   }
-togglePasswordMode() {
-  this.passwordTypeInput = this.passwordTypeInput === 'password' ? 'text' : 'password';
-  this.iconpassword = this.iconpassword === 'eye-off' ? 'eye' : 'eye-off';
-}
-private irParaPagina(pagina) {
-  this.navCtrl.navigateForward(pagina);
-}
+  togglePasswordMode() {
+    this.passwordTypeInput = this.passwordTypeInput === 'password' ? 'text' : 'password';
+    this.iconpassword = this.iconpassword === 'eye-off' ? 'eye' : 'eye-off';
+  }
+
+  private irParaPagina(pagina) {
+    this.navCtrl.navigateForward(pagina);
+  }
   irParaPaginaHome() {
     this.irParaPagina('home');
+  }
+  changeAuthAction() {
+    this.configs.cadastrar = !this.configs.cadastrar;
+    const logar = this.configs;
+    this.configs.action = logar ? 'Login' : 'Cadastre-se';
+    this.configs.actionChange = logar ? 'Cadastrar' : 'Já Sou Cadastrado';
+  }
+  ngOnInit(): void {
+    this.login();
+  }
+  private login(): void {
+    this.authForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.minLength(6),
+        Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+    });
+  }
 
+  get name(): FormControl {
+    return this.authForm.get('name') as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.authForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.authForm.get('password') as FormControl;
   }
 }
